c84298074bab9bdd02b14e99a448bef7c9e5139c
Trishiraj <gamerprince.exp@gmail.com>
2021-11-28 00:15:34 +0530

    Popout DisplayCard color with darker accent
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

--- components/DisplayCard.vue
+++ components/DisplayCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container column is-half mt-6">
-    <div class="card has-background-primary-light">
+    <div class="card has-background-grey-lighter">
       <header class="card-header">
         <p class="card-header-title">
           <b>Find Something to do</b>
@@ -14,7 +14,7 @@
         <strong class="mt-2 pl-4">Type:</strong> {{ event.type }}
       </div>
       <div class="card-footer pb-3 is-justify-content-end">
-        <button @click="$fetch" class="button mr-3 is-info">Get a new one</button>
+        <button @click="$fetch" class="button mr-3 mt-3 is-info">Get a new one</button>
       </div>
     </div>
   </div>
