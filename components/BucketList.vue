2a941d2ed784e925eb10ae83bccce093142f2da6
Trishiraj <gamerprince.exp@gmail.com>
2021-12-02 04:31:29 +0530

    Add a simple todo list with notification confirmation
    - We use a for loop to render static list items
    - With the help of v-model we get to add new todo item to the existing item
    - Also shows an example of how to use multiple function on click
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

---
+++ components/BucketList.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div class="container column is-half">
+      <button
+        @click="isListInputModalActive = true"
+        class="button is-info is-pulled-right mr-5"
+      >
+        ➕ Add item
+      </button>
+    </div>
+    <div class="section">
+      <div class="container column is-half">
+        <div
+          v-for="list in lists"
+          :key="list"
+          class="box has-background-success has-text-black"
+        >
+          {{ list }}
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal for List Input -->
+    <b-modal v-model="isListInputModalActive">
+      <div class="card container column is-half p-5">
+        <p class="is-size-3 has-text-centered">🗒️ Fill your bucket list!</p>
+        <hr class="has-background-grey-light" />
+        <div class="field">
+          <label class="label">Add your desired list item</label>
+          <div class="control">
+            <input
+              v-model="listitem"
+              class="input is-success"
+              type="text"
+              placeholder="Add your list item"
+            />
+          </div>
+        </div>
+        <div class="control">
+          <!-- We can declare multiple functions like this, semi-colon is the key here! -->
+          <button
+            @click="
+              submit();
+              isListInputModalActive = false;
+            "
+            class="button is-info"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      listitem: "",
+      lists: [
+        "I am currently learning Nuxt",
+        "This sets an example for list items",
+        "Until here, its just static strings looped using v-for",
+      ],
+      isListInputModalActive: false,
+    };
+  },
+  methods: {
+    submit() {
+        if(this.listitem){
+            this.lists.push(this.listitem)
+        }
+      this.$buefy.notification.open({
+        message: "New list item added successfully!",
+        type: "is-success",
+        position: "is-bottom-right"
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
--- pages/bucket-list.vue
+++ pages/bucket-list.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <pageHero />
-    Hello world
+    <BucketList />
   </div>
 </template>
 
