dda2a33fc912b7c2008272bec95f3e0786a6d13c
Trishiraj <gamerprince.exp@gmail.com>
2021-12-03 00:58:56 +0530

    Prettify DisplayName component
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

--- components/DisplayName.vue
+++ components/DisplayName.vue
@@ -2,7 +2,7 @@
   <div class="p-2 level">
     <div @click="isUserNameModalActive = true" class="level-item is-clickable">
       <figure class="image is-64x64">
-        <img class="is-rounded" src="@/static/avatar.png">
+        <img class="is-rounded" src="@/static/avatar.png" />
         <!-- We are using the v-show directive to show this element only if there's no name input entered.
         The property is passed through the modal->saved in data()->Checked using v-show -->
         <small v-show="!username" class="has-text-white">👆Click me</small>
@@ -16,26 +16,32 @@
     <b-modal v-model="isUserNameModalActive">
       <div class="card container column is-half is-mobile p-5">
         <p class="has-text-black">Set your Username🖋️</p>
-        <p><small>Close the modal to apply changes, The entered text will disappear when switching to a different tab as we are not using state management yet</small></p>
-        <input v-model="username" class="input is-success" type="text" placeholder="StarkDroid">
+        <p>
+          <small
+            >Close the modal to apply changes, The entered text will disappear
+            when switching to a different tab as we are not using state
+            management yet</small
+          >
+        </p>
+        <input
+          v-model="username"
+          class="input is-success"
+          type="text"
+          placeholder="StarkDroid"
+        />
       </div>
     </b-modal>
   </div>
 </template>
 <script>
 export default {
-data() {
-  return {
-    username: "",
-    isUserNameModalActive: false
-  }
-},
-methods: {
-  
-}
-}
+  data() {
+    return {
+      username: "",
+      isUserNameModalActive: false,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
