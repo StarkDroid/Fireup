4dc7e6e6638c8da81fffaad8c67433a593e11b3f
Trishiraj <gamerprince.exp@gmail.com>
2021-12-06 21:07:51 +0530

    Init Nuxt Firebase module
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,6 +41,24 @@ export default {
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios",
     "@nuxt/content",
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: 'AIzaSyCv5zbHvDKWwCt6feWoBauwWhinIDBbcvI',
+          authDomain: 'sigmaservice-a5620.firebaseapp.com',
+          databaseURL: 'https://sigmaservice-a5620.firebaseio.com',
+          projectId: 'sigmaservice-a5620',
+          storageBucket: 'sigmaservice-a5620.appspot.com',
+          messagingSenderId: '611425555532',
+          appId: '1:611425555532:web:27af51274e0dbd84df90e9',
+          measurementId: 'G-8T2DWWVPH7'
+        },
+        services: {
+          // Anything can be added here
+        }
+      }
+    ]
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
