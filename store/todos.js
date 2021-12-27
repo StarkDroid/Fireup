2c52d9c0b05235f8c8a2ddb649a02132d8a3853b
Trishiraj <gamerprince.exp@gmail.com>
2021-12-27 14:49:50 +0530

    Add a sample page to fit in Todo list based on Nuxt documentation
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

--- store/todos.js
+++ store/todos.js
@@ -1,11 +1,28 @@
 import Vue from 'vue'
 export const state = () => ({
-  todos: []
+  todos: [],
+  list: []
 })
 
 export const mutations = {
   addTodo(state, todo) {
     state.todos.push(todo)
+  },
+
+  add(state, text) {
+    state.list.push({
+      text,
+      done: false,
+      id: Date.now()
+    })
+  },
+
+  remove(state, todo) {
+    state.list = state.list.filter(item => item.id !== todo.id)
+  },
+
+  toggle(state, todo) {
+    todo.done = !todo.done
   },
 
   // This removes one item from the bottom of the list when triggered but not the exact item you want to remove
